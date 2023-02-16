e1fba508ceb038e2924880b25c5e1f0ecae7cc5e
Irisson N. Lima <irisson.lima@verdecard.com.br>
2023-02-15 23:39:13 -0300

    square digits

---
+++ exercicios/square_digits.rs
@@ -0,0 +1,20 @@
+fn square_digits(num: u64) -> u64 {
+    let mut divisor = num;
+    let mut resto:u64;
+    let mut order:u32 = 0;
+    let mut resp:u64 = 0;
+
+    while divisor > 0{
+        resto = divisor % 10;
+
+        resp += resto*resto * 10_i32.pow(order) as u64;
+        if resto>3{
+            order+=2;
+        }else{
+            order+=1;
+        }
+        divisor /= 10;
+    }
+
+    return resp;
+}
